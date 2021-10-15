1a4a1e2ac6fe86f45ee218e36c93d00906c65a92
Cassiana <lima.cglima@gmail.com>
2021-10-15 19:59:49 -0300

    quantificadores e intervalos

--- extrator_cep.py
+++ extrator_cep.py
@@ -1,10 +1,10 @@
-endereco = 'Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440120'
+endereco = 'Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120'
 
 import re # Regular Expression -- RegEx
 
 # 5 dígitos + hífen (opcional) + 3 digitos
 
-padrao = re.compile('[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]')
+padrao = re.compile('[0-9]{5}[-]{0,1}[0-9]{3}')
 busca = padrao.search(endereco)
 if busca:
     cep = busca.group()
