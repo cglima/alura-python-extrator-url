e2311b9d2507defcb82b10df339c1c1046e02de2
Cassiana <lima.cglima@gmail.com>
2021-10-14 21:29:45 -0300

    utilizando métodos de strings, URL com múltiplos parâmetros

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-url = 'bytebank.com/cambio?moedaOrigem=real'
+url = 'bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar'
 print(url)
 
-url_base = url[0:19]
-print(url_base)
-url_parametro = url[20:36]
-print(url_parametro)
+indice_interrogacao = url.find('?')
+url_base = url[0:indice_interrogacao]
+# print(url_base)
+url_parametros = url[indice_interrogacao + 1:]
+# print(url_parametros)
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
