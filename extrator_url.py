0d1ee672c5fdf52d51db48a522828ac0a2423fd1
Cassiana <lima.cglima@gmail.com>
2021-10-15 21:13:11 -0300

    desafio: conversão de moedas dólar e real

--- extrator_url.py
+++ extrator_url.py
@@ -50,13 +50,22 @@ class ExtratorURL:
     def __eq__(self, other):
         return self.url == other.url
 
-url = 'bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar'
+url = 'bytebank.com/cambio?quantidade=100&moedaOrigem=dolar&moedaDestino=real'
 extrator_url = ExtratorURL(url)
-extrator_url_2 = ExtratorURL(url)
 
-print(f'O tamanho da URL é: {len(extrator_url)}')
-print(f'URL completa: {extrator_url}')
-print(f'extrator_url == extrator_url_2? {extrator_url == extrator_url_2}')
-
+#conversão dolar pra real
+VALOR_DOLAR = 5.50 # 1 dólar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro('moedaOrigem')
+moeda_destino = extrator_url.get_valor_parametro('moedaDestino')
 valor_quantidade = extrator_url.get_valor_parametro('quantidade')
-print(f'Valor do parâmetro \"quantidade\": {valor_quantidade}')
+
+if moeda_origem == "real" and moeda_destino == "dolar":
+    valor_conversao = int(valor_quantidade) / VALOR_DOLAR
+    print("O valor de R$" + valor_quantidade + " reais é igual a $" + str(valor_conversao) + " dólares.")
+elif moeda_origem == "dolar" and moeda_destino == "real":
+    valor_conversao = int(valor_quantidade) * VALOR_DOLAR
+    print("O valor de $" + valor_quantidade + " dólares é igual a R$" + str(valor_conversao) + " reais.")
+else:
+    print(f"Câmbio de {moeda_origem} para {moeda_destino} não está disponível.")
+
+
